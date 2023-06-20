c014bfd89a1d298962507c95d4ad825b0d39e082
Hansen <hansen.zheng@hotmail.com>
2023-06-20 16:43:20 +0800

    feat(*): 更新，删除部分无用的代码

--- src/common/util.py
+++ src/common/util.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from copy import deepcopy
 
 
@@ -16,29 +15,3 @@ def deep_merge_dict(x, y):
             if isinstance(x[k], dict) and isinstance(y[k], dict):
                 z[k] = deep_merge_dict(x[k], y[k])
     return z
-
-
-def hundred_ns_to_string(hundred_ns: int) -> str:
-    """
-    百纳秒转可读字符串 HH:mm:ss
-    :return:
-    """
-    sec = hundred_ns // 10000000
-    min = sec // 60
-    hour = min // 60
-    return str(hour).zfill(2) + ":" + str(min % 60).zfill(2) + ":" + str(sec % 60).zfill(2)
-
-
-def byte_to_string(byte: int) -> str:
-    """
-    字节转可读字符串
-    :return:
-    """
-    if byte < 1024:
-        return str(byte) + " B"
-    elif byte < 1024 * 1024:
-        return str(round(byte / 1024, 2)) + " KB"
-    elif byte < 1024 * 1024 * 1024:
-        return str(round(byte / 1024 / 1024, 2)) + " MB"
-    else:
-        return str(round(byte / 1024 / 1024 / 1024, 2)) + " GB"
